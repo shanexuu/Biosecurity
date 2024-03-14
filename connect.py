5e3e61b4505754c3c64dcc56c5792c08b3bde27e
Shane Xu <139763196+shanexuu@users.noreply.github.com>
2024-03-14 13:02:03 +1300

    Delete connect.py

--- connect.py
+++
@@ -1,7 +0,0 @@
-# These details are available on the first MySQL Workbench screen
-# Usually called 'Local Instance'
-dbuser = "root" # Your MySQL username - likely 'root'
-dbpass = "wsXY6205506" # ---- PUT YOUR PASSWORD HERE ----
-dbhost = "localhost" 
-dbport = "3306"
-dbname = "biosecurity"
